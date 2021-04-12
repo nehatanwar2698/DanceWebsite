7098a2dea85fb1c674abdafd2491d7daa4a1bdf4
Neha <nehatanwar197@gmail.com>
2021-04-12 17:30:44 +0530

    added validation in contact form

--- app.js
+++ app.js
@@ -5,14 +5,37 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true, useUnifiedTopology: true});
 const port = 80;
 const bodyparser=require("body-parser");
+var validator=require('validator');
 
 // define mongoose schema 
 const contactSchema = new mongoose.Schema({
-    name: String,
-    phone: String,
-    email: String,
+    name:{
+        type:String,
+        required:true,
+        minLength:3
+    },
+    phone:{
+        type:Number,
+        required:true,
+        min:10
+     },
+    email: {
+        
+     type:String,
+     required:true,
+     validate(value){
+         if(!validator.isEmail(value)){
+             throw new Error("Invalid email id")
+         }
+     }
+},
     address: String,
-    desc: String
+    desc: String,
+    
+    date:{
+        type:Date,
+        default:Date.now
+    }
   });
 
   const Contact = mongoose.model('contact', contactSchema);
@@ -34,15 +57,15 @@ app.get('/', (req, res)=>{
 })
 
 app.get('/contact', (req, res)=>{
-   res.status(200).render('contact.pug');
+   res.status(200).render('contact');
 })
 // using express post request for save in database so install npm install body-parser 
 app.post('/contact', (req, res)=>{
     var myData=new Contact(req.body);
     myData.save().then(()=>{
-        res.send("this item has been save to the database")
-    }).catch(()=>{
-        res.status(400).send("item was not saved to database")
+        res.status(201).render("home")
+    }).catch((error)=>{
+        res.status(500).send(error)
     })
     // res.status(200).render('contact.pug');
  })
